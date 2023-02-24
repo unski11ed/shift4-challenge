edabe1b28690e4b67557e229d432f0112d0efaa4
Maciej Kurbański <maciej.kurbanski@outlook.com>
2023-02-24 00:38:05 +0000

    Fixed codesadbox error

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@emotion/react';
 import defaultTheme from '@app/theme';
 
 import './i18n';
-import App from './app';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
